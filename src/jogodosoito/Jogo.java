268420a8b9772c5347760effe7295e1173608794
Luis Guilherme Cipriani <cipriani.luisguilherme@gmail.com>
2022-09-14 17:14:45 -0300

    Cria método para a impressão da matriz

--- src/jogodosoito/Jogo.java
+++ src/jogodosoito/Jogo.java
@@ -13,6 +13,15 @@ public class Jogo {
 
     int[] linha = {1, 0, -1, 0};
     int[] coluna = {0, -1, 0, 1};
-    
+
     public int tamanho = 3;
+
+    public void imprimeMatriz(int[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
