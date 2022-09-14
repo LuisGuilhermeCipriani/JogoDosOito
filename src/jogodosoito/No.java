f09baf7b9d5a8caaa2f3a6864a5a6bd97b7f6bf0
Luis Guilherme Cipriani <cipriani.luisguilherme@gmail.com>
2022-09-14 16:41:43 -0300

    Cria atributos da classe No

--- src/jogodosoito/No.java
+++ src/jogodosoito/No.java
@@ -1,14 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package jogodosoito;
 
 /**
  *
- * @author cipri
+ * @author Luis Guilherme Cipriani
  */
 public class No {
+
+    public No vizinho;
     
+    int matriz[][] = new int[3][3];
+
+    public int coordX, coordY;
+
+    public int custo;
+
+    public int nivel;
 }
