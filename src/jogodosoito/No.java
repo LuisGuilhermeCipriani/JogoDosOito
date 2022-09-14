72b6a945c129be04c4543509dab97b11ae467664
Luis Guilherme Cipriani <cipriani.luisguilherme@gmail.com>
2022-09-14 16:57:28 -0300

    Cria método construtor da classe No

--- src/jogodosoito/No.java
+++ src/jogodosoito/No.java
@@ -1,4 +1,3 @@
-
 package jogodosoito;
 
 /**
@@ -8,7 +7,7 @@ package jogodosoito;
 public class No {
 
     public No vizinho;
-    
+
     int matriz[][] = new int[3][3];
 
     public int coordX, coordY;
@@ -16,4 +15,22 @@ public class No {
     public int custo;
 
     public int nivel;
+
+    public No(int matriz[][], int coordX, int coordY, int novaCoordX, int novaCoordY, No vizinho, int nivel) {
+
+        this.custo = Integer.MAX_VALUE;
+        this.nivel = nivel;
+        this.coordX = novaCoordX;
+        this.coordY = novaCoordY;
+        this.vizinho = vizinho;
+
+        this.matriz = new int[matriz.length][];
+        for (int i = 0; i < matriz.length; i++) {
+            this.matriz[i] = matriz[i].clone();
+        }
+
+        this.matriz[coordX][coordY] = this.matriz[coordX][coordY] + this.matriz[novaCoordX][novaCoordY];
+        this.matriz[novaCoordX][novaCoordY] = this.matriz[coordX][coordY] - this.matriz[novaCoordX][novaCoordY];
+        this.matriz[coordX][coordY] = this.matriz[coordX][coordY] - this.matriz[novaCoordX][novaCoordY];
+    }
 }
