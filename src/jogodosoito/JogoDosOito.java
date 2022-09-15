227d71b2d381e1e010450e6665a9d23fd24d6e59
Luis Guilherme Cipriani <cipriani.luisguilherme@gmail.com>
2022-09-15 12:17:56 -0300

    Adiciona comentários na classe JogoDosOito

--- src/jogodosoito/JogoDosOito.java
+++ src/jogodosoito/JogoDosOito.java
@@ -8,8 +8,12 @@ public class JogoDosOito {
 
     public static void main(String[] args) {
         
+        //Coordenadas do puzzle com espaço em branco na configuração inicial
         int coordX = 1, coordY = 0;
+        
+        //Configuração inicial, onde o valor 0 representa um espaço vazio
         int[][] configuracaoInicial = {{1, 8, 2}, {0, 4, 3}, {7, 6, 5}};
+        //Configuração final, ou seja, a matriz que se deseja alcançar, onde o valor 0 representa um espaço vazio
         int[][] configuracaoFinal = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
 
         Jogo jogo = new Jogo();
